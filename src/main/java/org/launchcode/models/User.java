d7067b56802c662a783c0fa08cec0053929a534f
schandalette <schanda.lynn@gmail.com>
2024-12-19 19:55:11 -0600

    spa user validation studio

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,35 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is Empty")
+    @Size(min = 5, max = 15, message = "Username must be 5-15 characters")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is Empty")
+    @Size(min = 6, message = "Password must be min 6 characters")
     private String password;
+
+    @NotBlank(message = "Password doesn't match")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -37,7 +53,24 @@ public class User {
     }
 
     public void setPassword(String password) {
+
         this.password = password;
+        checkPassword();
+    }
+
+    public  String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword(){
+        if(this.password != null && this.verifyPassword != null && !this.password.equals(this.verifyPassword)){
+            this.verifyPassword = null;
+        }
     }
 }
 
